82bf5a8173ca452ebeaf10ce92f3a33223dd8c9c
Zeineb <zeineb1mukhtar1@gmail.com>
2024-11-27 16:30:34 +0000

    4-get-article-by-id

--- app.js
+++ app.js
@@ -8,9 +8,22 @@ const app = express()
 
 const getApi = require("./controllers/api.controller")
 
+const {getArticleById} = require('./controllers/article_id.controller')
+
 // middleware
 app.use(express.json())
 
 app.get('/api', getApi)
 
+app.get('/api/articles/:article_id', getArticleById)
+
+
+
+
+
+app.use((err, req, res, next) => {
+    if (err.status && err.msg) {
+        return res.status(err.status).send({msg: err.msg})
+    }res.status(500).send({msg: 'Server error'})
+})
 module.exports = app
---
+++ controllers/article_id.controller.js
@@ -0,0 +1,18 @@
+const {fetchArticleById} = require('../article_id.model')
+
+
+const getArticleById = (req, res, next) => {
+    const {article_id} = req.params
+
+    if (typeof(article_id) !== 'string' || isNaN(Number(article_id))) { return next({ status: 400, msg: 'Bad request' }); }
+
+    fetchArticleById(article_id).then((article) => {
+        if (!article) {
+            return next({status: 404, msg: 'Article not found'})
+        }
+        res.status(200).send({article})
+    }).catch((err) => {
+        next({status: 500, msg: 'Server error'})
+    })
+}
+module.exports = {getArticleById}
