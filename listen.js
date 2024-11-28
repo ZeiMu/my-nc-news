c97010a7764683f3284818ffc488c19e15093367
Zeineb <zeineb1mukhtar1@gmail.com>
2024-11-28 13:16:50 +0000

    dotenv changed to dependency

--- listen.js
+++ listen.js
@@ -1,7 +1,12 @@
-const app = require('../app')
+// const app = require('../app')
 
-const {PORT = 3000} = process.env
+// const {PORT = 3000} = process.env
 
-app.listen(PORT, () => {
-    console.log("listening on PORT 3000")
-})
+// app.listen(PORT, () => {
+//     console.log("listening on PORT 3000")
+// })
+
+const app = require("./app.js");
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
